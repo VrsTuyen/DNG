4cb12408db7091856fd3e6ce63459b56d03986e2
Vrs Tuyen <vrs.tuyen@gmail.com>
2022-10-05 16:03:10 +0700

    5/10/2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
-var $ = document.querySelector.bind(document)
-var $$ = document.querySelectorAll.bind(document)
+// var $ = document.querySelector.bind(document)
+// var $$ = document.querySelectorAll.bind(document)
 
-const tabs = $$('.header-navbar__item')
-const contents = $$('.tab__item')
-console.log(tabs, contents)
+// const tabs = $$('.header-navbar__item')
+// const contents = $$('.tab__item')
+// console.log(tabs, contents)
 
-tabs.forEach( index => {
+// tabs.forEach( index => {
     
-});
+// });
 
-tabs.forEach(function(tab, index){
-    const content = contents[index]
-    tab.onclick = function(){
-        $('.header-navbar__item.header-navbar-item--active').classList.remove('header-navbar-item--active') 
-        $('.tab__item.active').classList.remove('active')
+// tabs.forEach(function(tab, index){
+//     const content = contents[index]
+//     tab.onclick = function(){
+//         $('.header-navbar__item.header-navbar-item--active').classList.remove('header-navbar-item--active') 
+//         $('.tab__item.active').classList.remove('active')
         
-        this.classList.add('header-navbar-item--active')
-        content.classList.add('active')
-    }
-})
+//         this.classList.add('header-navbar-item--active')
+//         content.classList.add('active')
+//     }
+// })
